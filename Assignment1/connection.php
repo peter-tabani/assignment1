828d83507af771e101ef09c4da9389eb3ca9853a
peter-tabani <peter.tabani@strathmore.edu>
2024-07-08 20:54:40 +0300

    initial

--- Assignment1/connection.php
+++ Assignment1/connection.php
@@ -4,12 +4,12 @@ $username = "root";
 $password = "";
 $dbname = "company";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$con = mysqli_connect($servername, $username, $password, $dbname);
 
 // Check connection
-if (!$conn) {
+if (mysqli_connect_errno()) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
 echo "Connected successfully";
-?>
+
