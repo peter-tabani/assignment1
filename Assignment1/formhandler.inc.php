82f816de09d51ecad0c15b3da84be11b3912e5dd
peter-tabani <peter.tabani@strathmore.edu>
2024-07-09 05:27:55 +0300

    CRUD

--- Assignment1/formhandler.inc.php
+++ Assignment1/formhandler.inc.php
@@ -34,9 +34,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } catch (PDOException $e) {
         die("Query failed: " . $e->getMessage());
     }
-} else {
-    header("Location: ../index.php");
-    die();
-}
+} 
 
 
